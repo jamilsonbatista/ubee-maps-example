ece2fa33d5000b3af65882bdcaec5cfaf2bb85ea
Gabriel Falcone <falconegabriel@gmail.com>
2014-01-21 11:58:21 -0300

    Correção na versão da API

--- src/in/ubee/example/maps/ExampleApplication.java
+++ src/in/ubee/example/maps/ExampleApplication.java
@@ -12,7 +12,6 @@ public class ExampleApplication extends Application {
     }
 
     private void ubeeSetup() {
-    	Ubee.setLogsVisible(true);
         Ubee.init(this);
         String mapsKey = "07be1c1963a2d5aab12dfb805a113bf28243a9c4c82136273af6b653ed989917";
         String mapsSecret = "5cf1baf2be27baad69d824de2a4d0af187db2c0065e0cf8766f351a880ef63e5";
